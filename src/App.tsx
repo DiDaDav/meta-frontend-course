c68bb25994dc5f1c1a6a3b04c909deb9cada1178
DIZY <david.dezy.dd@gmail.com>
2024-05-20 22:06:40 +0200

    🚧 define sections

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,92 @@ function App() {
   return (
     <>
       <div>
-        <h1>Homepage</h1>
+        <header>
+          <figure>
+            <img />
+            <figcaption></figcaption>
+          </figure>
+          <nav></nav>
+        </header>
+        <main>
+          <section>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>Laurem ipsum</p>
+            <figure>
+              <img />
+              <figcaption></figcaption>
+            </figure>
+            <button role='button'>Reserve a table</button>
+          </section>
+          <section>
+            <h1>Specials</h1>
+            <button role='button'>Online Meny</button>
+            <article>
+              <figure>
+                <img />
+                <figcaption></figcaption>
+              </figure>
+              <h1></h1>
+              <p></p>
+              <a></a>
+            </article>
+            <article>
+              <figure>
+                <img />
+                <figcaption></figcaption>
+              </figure>
+              <h1></h1>
+              <p></p>
+              <a></a>
+            </article>
+            <article>
+              <figure>
+                <img />
+                <figcaption></figcaption>
+              </figure>
+              <h1></h1>
+              <p></p>
+              <a></a>
+            </article>
+          </section>
+          <section>
+            <h1>Testimonials</h1>
+            <p>rating</p>
+            <p>user profile</p>
+            <figure>
+              <img />
+              <figcaption></figcaption>
+            </figure>
+            <p>review</p>
+          </section>
+          <section>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>Desc</p>
+            <aside>
+              <figure>
+                <img />
+                <figcaption></figcaption>
+              </figure>
+              <figure>
+                <img />
+                <figcaption></figcaption>
+              </figure>
+            </aside>
+          </section>
+        </main>
+        <footer>
+          <p>contact info</p>
+          <a>Social media links</a>
+          <nav>
+            <p>doormat nav</p>
+          </nav>
+          <figure>
+            <img />
+            <figcaption></figcaption>
+          </figure>
+        </footer>
       </div>
     </>
   )
