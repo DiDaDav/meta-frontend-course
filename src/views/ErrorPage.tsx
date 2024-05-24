186cd15f98801046ba944457229275ca349078a4
DIZY <david.dezy.dd@gmail.com>
2024-05-24 19:04:15 +0200

    ♻️ improve error page

--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -1,9 +1,28 @@
+import { useRouteError } from 'react-router-dom'
+import '../../src/css/error.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+type RouteError = {
+  status: number
+  statusText: string
+  internal: boolean
+  data: string
+  error: Error
+}
+
 const ErrorPage = () => {
+  const error = useRouteError() as RouteError
   return (
-    <main>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-    </main>
+    <>
+      <Header />
+      <main id='error-page'>
+        <h1>Oops! We encountered a {error.status} error</h1>
+        <h2>{error.statusText}</h2>
+        {!error.internal && <p>{error.data}</p>}
+      </main>
+      <Footer />
+    </>
   )
 }
 
