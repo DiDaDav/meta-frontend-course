0ea538a537066511025d40777833584b3e27fe9a
DIZY <david.dezy.dd@gmail.com>
2024-05-24 22:52:52 +0200

    💄 better header style

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <div className='container'>
+    <div className='container gy-4'>
       <Header />
       <main>
         <Outlet />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import Nav from './commons/Nav'
 import logoUrl from '../assets/logo_title.png'
 import mobileLogoUrl from '../assets/logo_complete_color.png'
 import { HStack, Image } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const menuList = [
   {
@@ -37,25 +38,31 @@ const menuList = [
 ]
 
 function Header() {
+  const itemList = menuList.map((item) => {
+    return (
+      <Link key={item.id ?? item.link} to={item.link} className='nav-link'>
+        {item.desc}
+      </Link>
+    )
+  })
+
   return (
-    <header className='row'>
-      <HStack>
-        <figure className='col'>
-          <picture>
-            <source media='(max-width:30em)' srcSet={mobileLogoUrl} />
-            <Image
-              id='logo'
-              className='img-fluid'
-              objectFit='contain'
-              width={{ base: '100%', md: '75%', lg: '50%', xl: '50%' }}
-              maxHeight={{ base: '150px' }}
-              src={logoUrl}
-              alt='Little Lemon logo'
-            />
-          </picture>
-        </figure>
-        <Nav list={menuList} />
-      </HStack>
+    <header className='row align-items-center'>
+      <figure className='figure col-3'>
+        <picture>
+          <source media='(max-width:576px)' srcSet={mobileLogoUrl} />
+          <Image
+            id='logo'
+            className='figure-img img-fluid'
+            objectFit='contain'
+            width={{ base: '100%', md: '75%', lg: '50%', xl: '50%' }}
+            maxHeight={{ base: '150px' }}
+            src={logoUrl}
+            alt='Little Lemon logo'
+          />
+        </picture>
+      </figure>
+      <nav className='col-9 nav nav-fill flex-column flex-sm-row align-items-center'>{itemList}</nav>
     </header>
   )
 }
