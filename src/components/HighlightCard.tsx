c3d3311c1b978467eb704eae242895b2f0f1fcc2
DIZY <david.dezy.dd@gmail.com>
2024-05-25 17:58:06 +0200

    💄 update higlight section

---
+++ src/components/HighlightCard.tsx
@@ -0,0 +1,47 @@
+import {
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Card as ChakraCard,
+  Image,
+} from '@chakra-ui/react'
+
+export type CardProps = {
+  title: string
+  price: string
+  description?: string
+  imageUrl: string
+}
+
+const Card = (cardProps: CardProps) => {
+  return (
+    <article className='col-9 col-sm-5 col-lg-2'>
+      <ChakraCard>
+        <CardHeader>
+          <Image
+            objectFit='fill'
+            src={cardProps.imageUrl}
+            width={{ base: '100%' }}
+            height={{ base: '25vh' }}
+            borderTopRadius={{ base: '16pt' }}
+          />
+        </CardHeader>
+        <CardBody>
+          <div className='row'>
+            <h1 className='card-title col'>{cardProps.title}</h1>
+            <p className='card-price col-4'>${cardProps.price}</p>
+          </div>
+          <p>
+            {cardProps.description ??
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+          </p>
+        </CardBody>
+        <CardFooter>
+          <a>Order a delivery +1</a>
+        </CardFooter>
+      </ChakraCard>
+    </article>
+  )
+}
+
+export default Card
--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,28 +1,46 @@
-import { Image } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import Card, { CardProps } from './HighlightCard'
+import greekSalad from '../assets/greek-salad.jpg'
+import bruschetta from '../assets/bruschetta.jpg'
+import frozenLemon from '../assets/frozen-lemon.jpg'
+
+const specials: CardProps[] = [
+  {
+    imageUrl: greekSalad,
+    title: 'Greek salad',
+    price: '12.99',
+  },
+  {
+    imageUrl: bruschetta,
+    title: 'Bruschetta',
+    price: '5.49',
+  },
+  {
+    imageUrl: frozenLemon,
+    title: 'Frozen Lemon',
+    price: '8.90',
+  },
+]
 
 function Highlights() {
   return (
-    <section id='highlights'>
-      <h1>Specials</h1>
-      <button role='button'>Online Menu</button>
-      <article>
-        <Image />
-        <h1></h1>
-        <p></p>
-        <a></a>
-      </article>
-      <article>
-        <Image />
-        <h1></h1>
-        <p></p>
-        <a></a>
-      </article>
-      <article>
-        <Image />
-        <h1></h1>
-        <p></p>
-        <a></a>
-      </article>
+    <section id='highlights' className='row'>
+      <div className='row align-items-center justify-content-center'>
+        <h1 className='section-title col-3'>Specials</h1>
+        <Button className='col-2' role='button'>
+          Online Menu
+        </Button>
+      </div>
+      <div className='row align-items-start justify-content-center'>
+        {specials.map((s) => (
+          <Card
+            imageUrl={s.imageUrl}
+            title={s.title}
+            price={s.price}
+            description={s.description}
+          />
+        ))}
+      </div>
     </section>
   )
 }
