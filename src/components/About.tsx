c1dc7d5214f772892fd9d3267572ec33bdc94025
DIZY <david.dezy.dd@gmail.com>
2024-05-25 19:26:06 +0200

    💄 add style

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,38 @@
 import { Image } from '@chakra-ui/react'
+import mario from '../assets/mario.png'
+import luigi from '../assets/Luigi.png'
 
 function About() {
   return (
-    <section id='about'>
-      <h1>Little Lemon</h1>
-      <h2>Chicago</h2>
-      <p>Desc</p>
-      <aside>
-        <Image />
-        <Image />
+    <section
+      id='about'
+      className='row align-items-center justify-content-evenly'
+    >
+      <div className='col-4 col-sm-4'>
+        <h1 className='title'>Little Lemon</h1>
+        <h2 className='subtitle'>Chicago</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <aside className='col-3'>
+        <figure className='figure col-4'>
+          <Image
+            className='col figure-img img-fluid'
+            objectFit='contain'
+            src={mario}
+            alt='Mario'
+          />
+        </figure>
+        <figure className='figure col-4'>
+          <Image
+            className='figure-img img-fluid'
+            objectFit='contain'
+            src={luigi}
+            alt='Lugi'
+          />
+        </figure>
       </aside>
     </section>
   )
--- src/views/BookingConfirmation.tsx
+++ src/views/BookingConfirmation.tsx
@@ -1,5 +1,12 @@
 function BookingConfirmation() {
-  return <h1>Congrats !</h1>
+  return (
+    <h1
+      className='text-center'
+      style={{ padding: '20%', backgroundColor: '#495E57', color: 'white' }}
+    >
+      Congrats !
+    </h1>
+  )
 }
 
 export default BookingConfirmation
